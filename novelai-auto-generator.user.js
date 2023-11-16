a828a4d9cd9799070d962e882d2df695ae50cf4e
BK927 <dead4bees927@gmail.com>
2023-11-17 04:40:33 +0900

    Update novelai-auto-generator.user.js
    add license

--- novelai-auto-generator.user.js
+++ novelai-auto-generator.user.js
@@ -7,6 +7,7 @@
 // @match        https://novelai.net/image
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=novelai.net
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 (function() {
